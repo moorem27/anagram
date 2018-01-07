511cac885ab592db62847200474b1e958daaa1f5
mmoore <moorem27@uw.edu>
2018-01-07 00:23:36 -0800

    Overload find_anagrams function

--- anagram.cc
+++ anagram.cc
@@ -37,6 +37,28 @@ std::map<int, std::set<std::string>> create_word_map( const std::string& file_pa
     return word_map;
 }
 
+std::set<std::string> find_anagrams( std::string& word, const std::string& file_path ) {
+    std::ifstream word_file( file_path, std::ios::binary );
+    std::string place_holder{};
+    std::set<std::string> anagrams{};
+    std::transform( word.begin(), word.end(), word.begin(), ::tolower );
+
+    if( word_file.is_open() ) {
+        while( std::getline( word_file, place_holder ) ) {
+            std::transform( place_holder.begin(), place_holder.end(), place_holder.begin(), ::tolower );
+            if( place_holder.size() == word.size() && std::is_permutation( word.begin(), word.end(), place_holder.begin() ) ) {
+                if( place_holder.compare( word ) != 0 ) 
+                    anagrams.insert( place_holder );
+            }
+            place_holder.clear();
+        }
+    }
+
+    word_file.close();
+
+    return anagrams;
+}
+
 std::set<std::string> find_anagrams( const std::string& word, std::map<int, std::set<std::string>>& word_map ) {
     std::set<std::string> anagrams{};
 
@@ -82,9 +104,7 @@ void get_all_anagrams( const std::string& word_file_path ) {
 int main( int argc, char* argv[] ) {
     if( argc == 2 ) {
         std::string search_word = std::string{ argv[ 1 ] };
-        std::transform( search_word.begin(), search_word.end(), search_word.begin(), ::tolower );
-        auto word_map = create_word_map( WORD_FILE );
-        auto anagrams = find_anagrams( search_word, word_map );
+        auto anagrams = find_anagrams( search_word, WORD_FILE );
 
         if( anagrams.size() == 0 ) {
             std::cout << "Nothing found!" << std::endl;
